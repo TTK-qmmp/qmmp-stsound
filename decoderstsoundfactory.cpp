9b07d12fd3d16d8731c647fbc65cefe4c4da1fa6
Greedysky <greedysky@163.com>
2026-03-20 21:56:37 +0800

    feat: Support for qmmp 2.4

--- decoderstsoundfactory.cpp
+++ decoderstsoundfactory.cpp
@@ -41,7 +41,7 @@ TrackInfoList DecoderStSoundFactory::createPlayList(const QString &path, TrackIn
 #if QMMP_VERSION_INT < 0x20400
     TrackInfo *raw(new TrackInfo(path)), *info = raw;
 #else
-    TrackInfo raw(path), info = &raw;
+    TrackInfo raw(path), *info = &raw;
 #endif
     if(parts == TrackInfo::Parts())
     {
